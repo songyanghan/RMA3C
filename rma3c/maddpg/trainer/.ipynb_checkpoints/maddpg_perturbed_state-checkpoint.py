66c9edb13e7700fc99e70a9dbf186650a6136ced
Songyang <49625592+songyanghan@users.noreply.github.com>
2023-01-31 22:15:28 -0500

    Update readme

--- rma3c/maddpg/trainer/.ipynb_checkpoints/maddpg_perturbed_state-checkpoint.py
+++ rma3c/maddpg/trainer/.ipynb_checkpoints/maddpg_perturbed_state-checkpoint.py
@@ -2,13 +2,18 @@ import numpy as np
 import random
 import tensorflow as tf
 import maddpg.common.tf_util as U
-import re
 
 from maddpg.common.distributions import make_pdtype
 from maddpg import AgentTrainer
 from maddpg.trainer.replay_buffer import ReplayBuffer
 import tensorflow.contrib.layers as layers
-import itertools as it
+import scipy.stats as stats
+
+def TruncatedNormal(mean = 0.0, std = 1.0, threshold = 1.0):
+    lower, upper = -threshold, threshold
+    mu, sigma = mean, std
+    X = stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+    return X
 
 def discount_with_dones(rewards, dones, gamma):
     discounted = []
@@ -36,15 +41,6 @@ def mlp_model_a(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=N
         out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=tf.nn.tanh)
         return out
 
-def mlp_model_p(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
-    # This model takes as input an observation and returns values of all actions
-    with tf.variable_scope(scope, reuse=reuse):
-        out = input
-        out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.relu)
-        out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.relu)
-        out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=tf.nn.softmax)
-        return out
-
 #adversary part, perturb the observated state
 def a_train(make_obs_ph_n, obs_shape_n, act_space_n, a_index, a_func, p_func, q_func, optimizer, d_value, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
@@ -54,7 +50,6 @@ def a_train(make_obs_ph_n, obs_shape_n, act_space_n, a_index, a_func, p_func, q_
         # set up placeholders
         obs_a_input = make_obs_ph_n
         act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action"+str(i)) for i in range(len(act_space_n))]
-        policy_q_acc = tf.placeholder(tf.float32, [None, int(act_pdtype_n[a_index].param_shape()[0])], name="policyQAcc")
         obs_size = int(obs_shape_n[a_index][0])
         
         a_input = obs_a_input[a_index]
@@ -63,44 +58,48 @@ def a_train(make_obs_ph_n, obs_shape_n, act_space_n, a_index, a_func, p_func, q_
         a_func_vars = U.scope_vars(U.absolute_scope_name("a_func"))
         
         # wrap parameters in distribution
-        a_reg = tf.reduce_mean(tf.square(a))
-           
         dc = tf.constant(d_value, dtype=tf.float32)
         perturbed_state = tf.add(a_input, tf.multiply(a, dc))
         
         p = p_func(perturbed_state, int(act_pdtype_n[a_index].param_shape()[0]), scope="p_func", reuse=True, num_units=num_units)
         act_pd = act_pdtype_n[a_index].pdfromflat(p)
-        ag_loss = tf.reduce_sum(tf.multiply(act_pd.sample(), policy_q_acc))
         
-        ascent_loss = -ag_loss #+ a_reg * 1e-3
+        act_input_n = act_ph_n + []
+        act_input_n[a_index] = act_pd.sample()
+        q_input = tf.concat(obs_a_input + act_input_n, 1)
+        if local_q_func:
+            q_input = tf.concat([obs_a_input[a_index], act_input_n[a_index]], 1)
+        q = q_func(q_input, 1, scope="q_func", reuse=True, num_units=num_units)[:,0]
+        pg_loss = -tf.reduce_mean(q)
+        
+        ascent_loss = -pg_loss #+ a_reg * 1e-3
         
         optimize_expr = U.minimize_and_clip(optimizer, ascent_loss, a_func_vars, grad_norm_clipping)
         
         # Create callable functions
-        train = U.function(inputs=obs_a_input + [policy_q_acc], outputs=ascent_loss, updates=[optimize_expr])
+        train = U.function(inputs=obs_a_input + act_ph_n, outputs=ascent_loss, updates=[optimize_expr])
         perturb_obs = U.function(inputs=[obs_a_input[a_index]], outputs = perturbed_state)
-        a_values = U.function([obs_a_input[a_index]], a)
-        
+        perturbed_obs = U.function([obs_a_input[a_index]], perturbed_state)
+
         # target network
         target_a = a_func(a_input, obs_size, scope="target_a_func", num_units=num_units)
         target_a_func_vars = U.scope_vars(U.absolute_scope_name("target_a_func"))
         update_target_a = make_update_exp(a_func_vars, target_a_func_vars)
-        
-        dc = tf.constant(d_value, dtype=tf.float32)
-        target_perturbed_state = tf.add(a_input, tf.multiply(target_a, dc))
-        target_perturb_obs = U.function(inputs=[obs_a_input[a_index]], outputs=target_perturbed_state)
 
-        return perturb_obs, train, update_target_a, {'a_values': a_values, 'perturb_obs': perturb_obs, 'target_perturb_obs': target_perturb_obs, 'a_func_vars':a_func_vars}
+        tdc = tf.constant(d_value, dtype=tf.float32)
+        target_perturbed_state = tf.add(a_input, tf.multiply(target_a, tdc))
+        target_perturbed_obs = U.function(inputs=[obs_a_input[a_index]], outputs=target_perturbed_state)
+
+        return perturb_obs, train, update_target_a, {'perturbed_obs': perturbed_obs, 'target_perturbed_obs': target_perturbed_obs}
         
 
-def p_train(make_obs_ph_n, obs_shape_n, act_space_n, p_index, p_func, a_func, optimizer, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
+def p_train(make_obs_ph_n, obs_shape_n, act_space_n, p_index, p_func, q_func, optimizer, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
         # create distribtuions
         act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
 
         # set up placeholders
         act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action"+str(i)) for i in range(len(act_space_n))]
-        policy_q_acc = tf.placeholder(tf.float32, [None, int(act_pdtype_n[p_index].param_shape()[0])], name="policyQAcc")
         perturbed_state = tf.placeholder(tf.float32, [None, int(obs_shape_n[p_index][0])], name="perturbed"+str(p_index))
 
         p = p_func(perturbed_state, int(act_pdtype_n[p_index].param_shape()[0]), scope="p_func", num_units=num_units)
@@ -111,27 +110,33 @@ def p_train(make_obs_ph_n, obs_shape_n, act_space_n, p_index, p_func, a_func, op
 
         act_sample = act_pd.sample()
         p_reg = tf.reduce_mean(tf.square(act_pd.flatparam()))
-        
-        pg_loss = tf.reduce_sum(tf.multiply(act_pd.sample(), policy_q_acc))
 
-        loss = pg_loss # + p_reg * 1e-3
+        act_input_n = act_ph_n + []
+        act_input_n[p_index] = act_pd.sample()
+        q_input = tf.concat(make_obs_ph_n + act_input_n, 1)
+        if local_q_func:
+            q_input = tf.concat([make_obs_ph_n[p_index], act_input_n[p_index]], 1)
+        q = q_func(q_input, 1, scope="q_func", reuse=True, num_units=num_units)[:,0]
+        pg_loss = -tf.reduce_mean(q)
+
+        loss = pg_loss + p_reg * 1e-3
 
         optimize_expr = U.minimize_and_clip(optimizer, loss, p_func_vars, grad_norm_clipping)
 
         # Create callable functions
-        train = U.function(inputs= [perturbed_state] + [policy_q_acc], outputs=loss, updates=[optimize_expr])
+        train = U.function(inputs=make_obs_ph_n + act_ph_n + [perturbed_state], outputs=loss, updates=[optimize_expr])
         act = U.function(inputs=[perturbed_state], outputs=act_sample)
         p_values = U.function([perturbed_state], p)
-        
+
         # target network
         target_p = p_func(perturbed_state, int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
         target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
         update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
-        
+
         target_act_sample = act_pdtype_n[p_index].pdfromflat(target_p).sample()
         target_act = U.function(inputs=[perturbed_state], outputs=target_act_sample)
 
-        return act, train, update_target_p, {'p_values': p_values, 'target_act': target_act, 'act': act, 'p_func_vars': p_func_vars}
+        return act, train, update_target_p, {'p_values': p_values, 'target_act': target_act}
 
 def q_train(make_obs_ph_n, act_space_n, q_index, q_func, optimizer, grad_norm_clipping=None, local_q_func=False, scope="trainer", reuse=None, num_units=64):
     with tf.variable_scope(scope, reuse=reuse):
@@ -168,43 +173,9 @@ def q_train(make_obs_ph_n, act_space_n, q_index, q_func, optimizer, grad_norm_cl
 
         target_q_values = U.function(obs_ph_n + act_ph_n, target_q)
 
-        return train, update_target_q, {'q_values': q_values, 'target_q_values': target_q_values, 'q_func_vars': q_func_vars}
+        return train, update_target_q, {'q_values': q_values, 'target_q_values': target_q_values}
 
-def BuildFullPermutation(act_num, agent_num):
-    identity = np.identity(act_num)
-    res = np.zeros(shape=(pow(act_num,agent_num),agent_num,act_num))
-    x1 = 0
-    x2 = 0
-    for i in it.product(identity, repeat=agent_num):
-        x2 = 0
-        for j in i:
-          res[x1, x2, :] = j
-          x2 = x2 + 1
-        x1 = x1 + 1
-    return res
-
-def ComputeCumofOtherAgentPolicy(act_prob_n, act_full_permut, obs_n, agents, index):
-    minBatchLen = np.shape(act_prob_n)[1]
-    agent_num = np.shape(act_prob_n)[0]
-    act_prob_n = np.array(act_prob_n)
-    act_num = np.shape(act_prob_n)[2]
-    act_prob_n = np.delete(act_prob_n, index, 0)
-    res = np.zeros(shape=(minBatchLen,act_num))
-    for permut in act_full_permut:
-        self_act = np.argwhere(permut[index,:] == 1.)[0,0]
-        action = np.expand_dims(permut,0).repeat(minBatchLen, axis=0)
-        action = action.transpose(1,0,2)
-        action_list = action.tolist()
-        action_t = np.delete(action, index, 0)
-        q = np.array([agents[j].q_debug['q_values'](*(obs_n + action_list)) for j in range(agent_num)])
-        q = np.delete(q, index, 0)
-        policy = np.sum(act_prob_n * action_t, axis = 2)
-        policy_q = np.prod(policy * q, axis = 0).flatten()
-        res[:,self_act] = res[:,self_act] + policy_q
-    return res        
-    
-    
-class RobustMADDPGAgentTrainer(AgentTrainer):
+class MADDPGPerturbedAgentTrainer(AgentTrainer):
     def __init__(self, name, model, obs_shape_n, obs_space_n, act_space_n, agent_index, args, local_q_func=False):
         self.name = name
         self.n = len(obs_shape_n)
@@ -232,9 +203,9 @@ class RobustMADDPGAgentTrainer(AgentTrainer):
             obs_shape_n = obs_shape_n,
             act_space_n=act_space_n,
             p_index=agent_index,
-            p_func=mlp_model_p,
-            a_func=mlp_model_a,
-            optimizer=tf.train.GradientDescentOptimizer(learning_rate=args.lr),
+            p_func=model,
+            q_func=model,
+            optimizer=tf.train.AdamOptimizer(learning_rate=args.lr),
             grad_norm_clipping=0.5,
             local_q_func=local_q_func,
             num_units=args.num_units
@@ -249,7 +220,7 @@ class RobustMADDPGAgentTrainer(AgentTrainer):
             a_func = mlp_model_a,
             p_func=model,
             q_func=model,
-            optimizer=tf.train.GradientDescentOptimizer(learning_rate=args.lr),
+            optimizer=tf.train.AdamOptimizer(learning_rate=args.lr),
             d_value = args.d_value,
             grad_norm_clipping=0.5,
             local_q_func=local_q_func,
@@ -259,8 +230,11 @@ class RobustMADDPGAgentTrainer(AgentTrainer):
         self.replay_buffer = ReplayBuffer(1e6)
         self.max_replay_buffer_len = args.batch_size * args.max_episode_len
         self.replay_sample_index = None
+        self.X = TruncatedNormal(std = args.noise_std)
         
     def perturb(self, obs):
+        if self.args.noise_type != 0:
+            return self.obs(self.addNoiseForOneAgent(obs[None]))
         return self.obs(obs[None])
 
     def action(self, obs):
@@ -272,6 +246,28 @@ class RobustMADDPGAgentTrainer(AgentTrainer):
 
     def preupdate(self):
         self.replay_sample_index = None
+        
+    def ComputeTargetPerturbedObs(self, origin_obs, agents):
+        obs_array = []
+        for i in range(self.n):
+            perturbed_state = agents[i].a_debug['target_perturbed_obs'](origin_obs[i])
+            obs_array.append(perturbed_state)
+        return obs_array
+    
+    def addNoise(self, origin_obs):
+        obs_array = []
+        for i in range(self.n):
+            temp = np.array(origin_obs[i])
+            noise = self.X.rvs(np.size(temp)) * self.args.d_value
+            noise.shape = temp.shape
+            obs_array.append((temp+noise).tolist())
+        return obs_array
+    
+    def addNoiseForOneAgent(self, origin_obs):
+        temp = np.array(origin_obs)
+        noise = self.X.rvs(np.size(temp)) * self.args.d_value
+        noise.shape = temp.shape
+        return (temp+noise).tolist()
 
     def update(self, agents, t):
         if len(self.replay_buffer) < self.max_replay_buffer_len: # replay buffer is not large enough
@@ -295,24 +291,26 @@ class RobustMADDPGAgentTrainer(AgentTrainer):
         # train q network
         num_sample = 1
         target_q = 0.0
-        perturbed_next_obs = [agents[i].a_debug['target_perturb_obs'](obs_next_n[i]) for i in range(self.n)]
-        target_act_next_n = [agents[i].p_debug['target_act'](perturbed_next_obs[i]) for i in range(self.n)]
+        noise_obs_next_n = obs_next_n
+        if self.args.noise_type != 0:
+            noise_obs_next_n = self.addNoise(noise_obs_next_n)
+        perturbed_obs = self.ComputeTargetPerturbedObs(noise_obs_next_n, agents)
+        target_act_next_n = [agents[i].p_debug['target_act'](perturbed_obs[i]) for i in range(self.n)]
         target_q_next = self.q_debug['target_q_values'](*(obs_next_n + target_act_next_n))
         target_q += rew + self.args.gamma * (1.0 - done) * target_q_next
         q_loss = self.q_train(*(obs_n + act_n + [target_q]))
-
+        
         # gradient descent ascent inner loop
-        perturbed_cur_obs = [agents[i].a_debug['perturb_obs'](obs_n[i]) for i in range(self.n)]
-        act_prob_n = [agents[i].p_debug['act'](perturbed_cur_obs[i]) for i in range(self.n)]
-        act_full_permut = BuildFullPermutation(np.shape(act_n)[2], self.n)
-        policy_q_cum = ComputeCumofOtherAgentPolicy(act_prob_n, act_full_permut, obs_n, agents, self.agent_index).tolist()
+        noise_obs_n = obs_n
+        if self.args.noise_type != 0:
+            noise_obs_n = self.addNoise(noise_obs_n)
         for i in range(self.args.gda_step):
             # train p network
-            perturbed_obs = [agents[i].a_debug['perturb_obs'](obs_n[i]) for i in range(self.n)]
-            p_loss = self.p_train(*([perturbed_obs[self.agent_index]] + [policy_q_cum]))
+            cur_perturbed_obs = [agents[self.agent_index].a_debug['perturbed_obs'](noise_obs_n[self.agent_index])]
+            p_loss = self.p_train(*(obs_n + act_n + cur_perturbed_obs))
         
             #train a network
-            a_loss = self.a_train(*(obs_n + [policy_q_cum]))
+            a_loss = self.a_train(*(obs_n + act_n))
 
         self.p_update()
         self.q_update()
